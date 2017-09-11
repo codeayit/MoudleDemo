2a8f6e13671e746e5d64e1ef48cca8fe5e9fbac0
lny <1053452188@qq.com>
2017-09-11 14:46:09 +0800

    Initial commit

---
+++ testlibrary/src/main/java/com/ayit/testlibrary/mode/User.java
@@ -0,0 +1,53 @@
+package com.ayit.testlibrary.mode;
+
+/**
+ * Created by lny on 2017/9/11.
+ */
+
+public class User {
+    private String name;
+    private int age;
+    private String addr;
+
+    public User() {
+    }
+
+    public User(String name, int age, String addr) {
+        this.name = name;
+        this.age = age;
+        this.addr = addr;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", addr='" + addr + '\'' +
+                '}';
+    }
+}
